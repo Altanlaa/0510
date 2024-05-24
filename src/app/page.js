12b89518803246738116cd2f84054773b19abbce
Bubu <b11117029@gemail.yuntech.edu.tw>
2024-05-24 16:55:07 +0800

    cc2

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+/*
+ * @author Altanla <altan0225@gmail.com> 
+ */
 
 import Link from "next/link";
 import { Navbar,
@@ -15,6 +18,25 @@ import { Navbar,
       DarkThemeToggle } from "flowbite-react";
 
 export default function Home() {
+
+  const items = [
+    {
+      cover:"/banner/ban-1.jpg",
+      name:"toasts",
+      description:"oh..burn",
+    },
+    {
+      cover:"/banner/ban-2.jpg",
+      name:"sad toast",
+      description:"so sad :(",
+    },
+    {
+      cover:"/banner/ban-3.jpg",
+      name:"watching toast",
+      description:"toast is watching u",
+    },
+  ];
+
   return (
     <>
     <div className="bg-cyan-400">{/*also-> bg-[#ff00ff] */}
@@ -45,26 +67,29 @@ export default function Home() {
     </div>
 
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slide={false}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+      <Carousel>
+        <img src="/banner/ban-1.jpg" alt="copyright of kim huynh" />
+        <img src="/banner/ban-2.jpg" alt="copyright of Kenneth Lu" />
+        <img src="/banner/ban-3.jpg" alt="copyright of Quinn Dombrowski" />
+        <img src="/banner/ban-4.jpg" alt="copyright  of Quinn Dombrowski" />
         <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
       </Carousel>
     </div>
 
-    <div className="bg-white">
-    <div className="container mx-auto">
-    <Card className="max-w-sm">
+    <div className="bg-white py-16">
+    <div className="container mx-auto grid grid-cols-4 gap-8">
+      {items.map( item=>
+    <Card className="max-w-sm"
+      imgAlt={item.name}
+      imgSrc={item.cover}>
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
+        {item.name}
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        {item.description}
       </p>
       <Button>
-        Read more
+        watch it
         <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path
             fillRule="evenodd"
@@ -74,6 +99,7 @@ export default function Home() {
         </svg>
       </Button>
     </Card>
+      )}
     </div>
     </div>
 
