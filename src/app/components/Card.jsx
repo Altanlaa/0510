3d6faf18950120f2ea4d559e34ce191b3ae10056
Altanlaa <altan0225@gmail.com>
2024-05-31 17:02:31 +0800

    0531

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,11 @@
+/*
+ * @author Altanla <altan0225@gmail.com> 
+ */
+
+export default function Customcard({items}){
+    return (
+        <div className="border">
+            <div>{items.ScenicSpotName}</div>
+        </div>
+    )
+}
